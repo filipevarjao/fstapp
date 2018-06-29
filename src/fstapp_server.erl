926decacef25f6fc4c0c4fc708192c597cf70370
Filipe <varjaofilipe@gmail.com>
2018-06-29 13:52:11 -0300

    stop gettting metrics

--- src/fstapp_server.erl
+++ src/fstapp_server.erl
@@ -1,32 +1,42 @@
 -module(fstapp_server).
 -behaviour(gen_server).
 
--export([start_link/0]).
+-export([start_link/0, stop_get_metr/0]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
 	terminate/2]).
+
+-record(state, {timer_ref}).
 
 start_link() ->
 	gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 init([]) ->
-	erlang:send_after(5000, self(), collect_metrics),
-	{ok, #{}}.
+	TimerRef = erlang:send_after(5000, self(), collect_metrics),
+	{ok, #state{timer_ref=TimerRef}}.
 
 handle_call(_Request, _From, State) ->
 	Reply = ok,
 	{reply, Reply, State}.
 
+handle_cast(stop, State) ->
+	erlang:cancel_timer(State#state.timer_ref),
+	{noreply, State};
 handle_cast(_Msg, State) ->
 	{noreply, State}.
 
 handle_info(collect_metrics, State) ->
-	Cpu = cpu_sup:nprocs(),
+	io:format("~p~n", [State#state.timer_ref]),
+	ProcessCount = cpu_sup:nprocs(),
 	{Total, Alloc, _} = memsup:get_memory_data(),
-	io:format("Number of processes running on this machine: ~p~n", [Cpu]),
+	io:format("Number of processes running on this machine: ~p~n", [ProcessCount]),
 	io:format("Using ~p of memory from a total of ~p ~n", [Alloc, Total]),
-	erlang:send_after(5000, self(), collect_metrics),
+	TimerRef = erlang:send_after(5000, self(), collect_metrics),
+	{noreply, #state{timer_ref=TimerRef}};
+handle_info(_, State) ->
 	{noreply, State}.
 
 terminate(_Reason, _State) ->
 	ok.
 
+stop_get_metr() ->
+	gen_server:cast(?MODULE, stop).
