c2c10b528e7e7a570843de084ecafa4cc636708f
Filipe <varjaofilipe@gmail.com>
2018-06-29 17:33:04 -0300

    refactoring the guard in the integer check

--- src/fstapp.erl
+++ src/fstapp.erl
@@ -4,11 +4,11 @@
 start_metrics() ->
 	fstapp_server:start_get_metr().
 
-change_frequency(Time) ->
-	case is_integer(Time) of
-		true -> fstapp_server:change_freq_metr(Time);
-		false -> io:format("You must pass an integer as argument.~n")
-	end.
+change_frequency(Time) when is_integer(Time) ->
+	fstapp_server:change_freq_metr(Time);
+change_frequency(_) ->
+	io:format("You must pass an integer as argument.~n"),
+	{error, not_integer}.
 
 stop_metrics() ->
 	fstapp_server:stop_get_metr().
