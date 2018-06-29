c933e76a18f627e66f4064e558484bea1ab70c9d
Filipe <varjaofilipe@gmail.com>
2018-06-29 16:56:10 -0300

    add update frequency

--- src/fstapp.erl
+++ src/fstapp.erl
@@ -1,8 +1,11 @@
 -module(fstapp).
--export([start_metrics/0, stop_metrics/0]).
+-export([start_metrics/0, change_frequency/1, stop_metrics/0]).
 
 start_metrics() ->
 	fstapp_server:start_get_metr().
+
+change_frequency(Time) ->
+	fstapp_server:change_freq_metr(Time).
 
 stop_metrics() ->
 	fstapp_server:stop_get_metr().
--- src/fstapp_server.erl
+++ src/fstapp_server.erl
@@ -1,24 +1,28 @@
 -module(fstapp_server).
 -behaviour(gen_server).
 
--export([start_link/0, start_get_metr/0, stop_get_metr/0]).
+-export([start_link/0, start_get_metr/0, change_freq_metr/1, stop_get_metr/0]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
 	terminate/2]).
 
--record(state, {timer_ref}).
+-record(state, {timer_ref, freq}).
 
 start_link() ->
 	gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 init([]) ->
 	TimerRef = erlang:send_after(5000, self(), collect_metrics),
-	{ok, #state{timer_ref=TimerRef}}.
+	{ok, #state{timer_ref=TimerRef, freq=5000}}.
 
 handle_call(start_collecting_metrics, _From, State) ->
 	erlang:cancel_timer(State#state.timer_ref),
-	TimerRef = erlang:send_after(5000, self(), collect_metrics),
+	TimerRef = erlang:send_after(State#state.freq, self(), collect_metrics),
 	Reply = ok,
-	{reply, ok, #state{timer_ref=TimerRef}};
+	{reply, ok, #state{timer_ref=TimerRef, freq=State#state.freq}};
+handle_call({update_freq_metrics, Time}, _From, State) ->
+	erlang:cancel_timer(State#state.timer_ref),
+	TimerRef = erlang:send_after(Time*1000, self(), collect_metrics),
+	{reply, ok, #state{timer_ref=State#state.timer_ref, freq=Time*1000}};
 handle_call(stop_collecting_metrics, _From, State) ->
 	erlang:cancel_timer(State#state.timer_ref),
 	{reply, ok, State}.
@@ -26,13 +30,13 @@ handle_call(stop_collecting_metrics, _From, State) ->
 handle_cast(_Msg, State) ->
 	{noreply, State}.
 
-handle_info(collect_metrics, _State) ->
+handle_info(collect_metrics, State) ->
 	ProcessCount = cpu_sup:nprocs(),
 	{Total, Alloc, _} = memsup:get_memory_data(),
 	io:format("Number of processes running on this machine: ~p~n", [ProcessCount]),
 	io:format("Using ~p of memory from a total of ~p ~n", [Alloc, Total]),
-	TimerRef = erlang:send_after(5000, self(), collect_metrics),
-	{noreply, #state{timer_ref=TimerRef}};
+	TimerRef = erlang:send_after(State#state.freq, self(), collect_metrics),
+	{noreply, #state{timer_ref=TimerRef, freq=State#state.freq}};
 handle_info(_, State) ->
 	{noreply, State}.
 
@@ -41,6 +45,9 @@ terminate(_Reason, _State) ->
 
 start_get_metr() ->
 	gen_server:call(?MODULE, start_collecting_metrics).
+
+change_freq_metr(Time) ->
+	gen_server:call(?MODULE,{ update_freq_metrics, Time}).
 
 stop_get_metr() ->
 	gen_server:call(?MODULE, stop_collecting_metrics).
