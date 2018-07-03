b9289db4b6c90b25bb486cd4e9915a0b40ba0a3a
Filipe <varjaofilipe@gmail.com>
2018-07-03 15:53:54 -0300

    collecting more about cpu usage and disk utilization

--- src/fstapp_server.erl
+++ src/fstapp_server.erl
@@ -1,7 +1,8 @@
 -module(fstapp_server).
 -behaviour(gen_server).
 
--export([start_link/0, start_get_metrics/0, change_freq_metrics/1, stop_get_metrics/0]).
+-export([start_link/0, start_get_metrics/0, change_freq_metrics/1, stop_get_metrics/0,
+	 metrics/1]).
 
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
 	terminate/2]).
@@ -35,11 +36,10 @@ init([]) ->
 handle_call(start_get_metrics, _From, State) ->
 	erlang:cancel_timer(State#state.timer_ref),
 	TimerRef = erlang:send_after(State#state.freq, self(), collect_metrics),
-	Reply = ok,
 	{reply, ok, #state{timer_ref=TimerRef, freq=State#state.freq}};
 handle_call({change_freq_metrics, Time}, _From, State) ->
 	erlang:cancel_timer(State#state.timer_ref),
-	TimerRef = erlang:send_after(Time*1000, self(), collect_metrics),
+	erlang:send_after(Time*1000, self(), collect_metrics),
 	{reply, ok, #state{timer_ref=State#state.timer_ref, freq=Time*1000}};
 handle_call(stop_collecting_metrics, _From, State) ->
 	erlang:cancel_timer(State#state.timer_ref),
@@ -49,16 +49,24 @@ handle_cast(_Msg, State) ->
 	{noreply, State}.
 
 handle_info(collect_metrics, State) ->
-	ProcessCount = cpu_sup:nprocs(),
-	{Total, Alloc, _} = memsup:get_memory_data(),
-	io:format("Number of processes running on this machine: ~p~n", [ProcessCount]),
-	io:format("Using ~p of memory from a total of ~p ~n", [Alloc, Total]),
-	erlang:cancel_timer(State#state.timer_ref),
-	TimerRef = erlang:send_after(State#state.freq, self(), collect_metrics),
+	TimerRef = metrics(State),
 	{noreply, State#state{timer_ref=TimerRef}};
 handle_info(_, State) ->
 	{noreply, State}.
 
 terminate(_Reason, _State) ->
 	ok.
+
+metrics(State) ->
+	ProcessCount = cpu_sup:nprocs(),
+	CpuUtil = cpu_sup:util(),
+	{Total, Alloc, _} = memsup:get_memory_data(),
+	MemDataList = memsup:get_system_memory_data(),
+	DiskUsed = disksup:get_almost_full_threshold(),
+	io:format("CPU utilization ~p%~n", [CpuUtil]),
+	io:format("Percentage of disk space utilization ~p%~n", [DiskUsed]),
+	io:format("Number of processes running on this machine: ~p~n", [ProcessCount]),
+	io:format("Using ~p of memory from a total of ~p ~n", [Alloc, Total]),
+	erlang:cancel_timer(State#state.timer_ref),
+	erlang:send_after(State#state.freq, self(), collect_metrics).
 
