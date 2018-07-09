5a289541aceb4bc2a4a5cb01d3a92584f4755855
Filipe Varjão <varjaofilipe@gmail.com>
2018-07-09 12:11:45 -0300

    Frequency only accept milliseconds now, test case updated

--- test/fstapp_SUITE.erl
+++ test/fstapp_SUITE.erl
@@ -5,9 +5,11 @@
 -compile(export_all).
 
 init_per_suite(Config) ->
+	application:ensure_all_started(fstapp),
 	Config.
 
 end_per_suite(Config) ->
+	application:stop(fstapp),
 	Config.
 
 all() ->
@@ -15,15 +17,13 @@ all() ->
 
 my_test_case(_Config) ->
 
-	application:ensure_all_started(fstapp),
-
 	ct:log(start, "The server start by collecting metrics with frequency value as 5ms~n"),
 	CurrentFreq = 5000, % It represents the time in milliseconds.
 	CurrentFreq = fstapp:get_current_frequency(),
 
 	ct:log(change_frequency, "Update the frequency to 10ms~n"),
-	{NewFreq, Ms} = {10, 10000},
+	NewFreq = 10000,
 	ok = fstapp:change_frequency(NewFreq),
-	Ms = fstapp:get_current_frequency().
+	NewFreq = fstapp:get_current_frequency().
 
 
