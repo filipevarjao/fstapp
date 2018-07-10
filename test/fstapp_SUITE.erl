739d0bfccccaf75f20b8bbb25c96d2308eb6177b
Filipe <varjaofilipe@gmail.com>
2018-07-10 17:36:59 -0300

    I move the register to inside the test case, and it print the process correctly. I think it works because there is a delay to collect and send the metrics. But then the keyfind fail, I changed to lists:keysearch

--- test/fstapp_SUITE.erl
+++ test/fstapp_SUITE.erl
@@ -5,37 +5,36 @@
 -compile(export_all).
 
 init_per_suite(Config) ->
-	true = register(fstapp_SUITE_process, self()),
-	application:ensure_all_started(fstapp),
-	Config.
+        application:ensure_all_started(fstapp),
+        Config.
 
 end_per_suite(Config) ->
-	application:stop(fstapp),
-	Config.
+        application:stop(fstapp),
+        Config.
 
 all() ->
-	[my_test_case].
+        [my_test_case].
 
 my_test_case(_Config) ->
 
-	{ok, Module} = application:get_env(fstapp, callback_module),
+        true = register(fstapp_SUITE_process, self()),
 
-	receive
-		Metrics ->
-			{cpu, _} = erlang:keyfind(cpu, 1, Metrics),
-			{ostype, _} = erlang:keyfind(ostype, 1, Metrics),
-			{proc, _} = erlang:keyfind(proc, 1, Metrics),
-			{disk, _} = erlang:keyfind(disk, 1, Metrics)
+        receive
+                Metrics ->
+                        {cpu, _} = lists:keysearch("cpu", 1, Metrics)
+        %               {cpu, _} = erlang:keyfind(cpu, 1, Metrics),
+        %               {ostype, _} = erlang:keyfind(ostype, 1, Metrics),
+        %               {proc, _} = erlang:keyfind(proc, 1, Metrics),
+        %               {disk, _} = erlang:keyfind(disk, 1, Metrics)
 
-	end,
+        end,
 
-	ct:log(start, "The server start by collecting metrics with frequency value as 5ms~n"),
-	CurrentFreq = 5000, % It represents the time in milliseconds.
-	CurrentFreq = fstapp:get_current_frequency(),
+        ct:log(start, "The server start by collecting metrics with frequency value as 5ms~n"),
+        CurrentFreq = 5000, % It represents the time in milliseconds.
+        CurrentFreq = fstapp:get_current_frequency(),
 
-	ct:log(change_frequency, "Update the frequency to 10ms~n"),
-	NewFreq = 10000,
-	ok = fstapp:change_frequency(NewFreq),
-	NewFreq = fstapp:get_current_frequency().
-
+        ct:log(change_frequency, "Update the frequency to 10ms~n"),
+        NewFreq = 10000,
+        ok = fstapp:change_frequency(NewFreq),
+        NewFreq = fstapp:get_current_frequency().
 
