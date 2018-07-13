3edbc59dffcf930571ed34bd21170b5d0a79121a
Filipe <varjaofilipe@gmail.com>
2018-07-13 15:42:42 -0300

    almost there, I think so

--- src/fstapp_tcp_handler.erl
+++ src/fstapp_tcp_handler.erl
@@ -5,7 +5,7 @@
 -export([handle_init/0, handle_data/2]).
 
 handle_init() ->
-	gen_tcp:connect("localhost", 5678, []).
+	gen_tcp:connect("localhost", 5678, [binary, {packet, 0}]).
 
 handle_data(Metrics, Sock) ->
 	gen_tcp:send(Sock, Metrics),
--- test/fstapp_tcp_SUITE.erl
+++ test/fstapp_tcp_SUITE.erl
@@ -9,17 +9,18 @@ init_per_testcase(_, Config) ->
 	ok = application:set_env(fstapp, callback_module, fstapp_tcp_handler),
 	{ok, fstapp_tcp_handler} = application:get_env(fstapp, callback_module),
 	{ok, _} = application:ensure_all_started(fstapp),
-	Config.
+	{ok, LSock} = gen_tcp:listen(5678, [binary, {packet, 0}, 
+                                        {active, false}]),
+	[ {lsock, LSock } | Config].
 
 end_per_testcase(_, _Config) ->
 	ok = application:stop(fstapp).
 
 all() -> [tcp_test].
 
-tcp_test(_Config) ->
-	{ok, LSock} = gen_tcp:listen(5678, [list, {packet, 0},
-			{active, false}, {reuseaddr, true}]),
+tcp_test(Config) ->
+	LSock = ?config(lsock, Config),
 	{ok,Sock} = gen_tcp:accept(LSock),
-	{ok, _} = gen_tcp:recv(Sock, 0),
+	{ok, _Metrics} = gen_tcp:recv(Sock, 0),
 	ok = gen_tcp:close(Sock),
 	ok = gen_tcp:close(LSock).
